30a515759d54600dac10755e57e142a6d8fb43f6
Juann651 <juansantosleal651@gmail.com>
2025-05-13 20:42:33 -0300

    Add files via upload

---
+++ Aula 18/script.js
@@ -0,0 +1,6 @@
+
+            document.write("<h3>Ola, mundo!</h3>");
+            /* window.alert("Ola, mundo");*/
+            /* document.getElementById("resposta").innerHTML = window.
+            confirm("Voce esta gostando do curso?");'p'*/
+            document.getElementById("respostas").innerHTML = "O meu nome é " + window.prompt("Qual é o seu nome?");
